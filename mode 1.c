af5c81ab48c80fea61ece9751a7c17063c9fb693
Yaqi-Z <yaqizhen424@gmail.com>
2024-03-26 21:44:41 -0400

    mode 1 decleartion

---
+++ mode 1.c
@@ -0,0 +1,5 @@
+// goal: mode 1: catch more fruit as you can 
+// Game over when: 1. catch a bomb, 2 loosing 3 fruits
+
+// Fruits would drop at constant speed with random place, the speed of fruit dropping is not changing for now 
+// However, the num of the bombs will increase while time goes while num of fruits is not changing.
