b3561bd1f05ae8e43b0fa66b94711d04de9a4d8a
Shirley Li <shirleyy.li@outlook.com>
2024-03-29 20:46:27 -0400

    normal fruit dropping ok, interrupt not working

--- code/main.c
+++ code/main.c
@@ -6,6 +6,7 @@
 #include <graphics.h>
 
 
+
 /* Cyclone V FPGA devices */
 #define LEDR_BASE             0xFF200000
 #define HEX3_HEX0_BASE        0xFF200020
@@ -15,6 +16,28 @@
 #define TIMER_BASE            0xFF202000
 #define PIXEL_BUF_CTRL_BASE   0xFF203020
 #define CHAR_BUF_CTRL_BASE    0xFF203030
+	
+// Control Register for Interrupt
+
+/*#ifndef __NIOS2_CTRL_REG_MACROS__
+#define __NIOS2_CTRL_REG_MACROS__
+#define NIOS2_READ_STATUS(dest) \
+do { dest = __builtin_rdctl(0); } while (0)
+#define NIOS2_WRITE_STATUS(src) \
+do { __builtin_wrctl(0, src); } while (0)
+#define NIOS2_READ_ESTATUS(dest) \
+do { dest = __builtin_rdctl(1); } while (0)
+#define NIOS2_READ_BSTATUS(dest) \
+do { dest = __builtin_rdctl(2); } while (0)
+#define NIOS2_READ_IENABLE(dest) \
+do { dest = __builtin_rdctl(3); } while (0)
+#define NIOS2_WRITE_IENABLE(src) \
+do { __builtin_wrctl(3, src); } while (0)
+#define NIOS2_READ_IPENDING(dest) \
+do { dest = __builtin_rdctl(4); } while (0)
+#define NIOS2_READ_CPUID(dest) \
+do { dest = __builtin_rdctl(5); } while (0)
+#endif*/
 
 /* VGA colors */
 #define WHITE 0xFFFF
@@ -41,6 +64,8 @@
 #define FALSE 0
 #define TRUE 1
 	
+#define COUNTER_VALUE 50000
+	
 
 int resolution_x, resolution_y; 							// VGA screen size
 int sizeof_pixel;						// number of bytes per pixel
@@ -58,7 +83,12 @@ int fruit_x_pos;
 int prev_fruit_x, prev_fruit_y; 	// x, y coordinates of boxes to draw
 int prev_prev_fruit_x, prev_prev_fruit_y; 	// x, y coordinates of boxes to draw
 
-int ran_num;
+int rand_fruit;
+int prev_rand_fruit;
+int prev_prev_rand_fruit;
+int drop_speed;
+
+int total_score;
 
 int* fruit_map[] = {watermelon_map, orange_map, apple_map, grape_map, peach_map, mandarin_map, banana_map, blueberry_map, strawberry_map};
 int* fruit_width[] = {WATERMELON_WIDTH, ORANGE_WIDTH, APPLE_WIDTH, GRAPE_WIDTH, PEACH_WIDTH, MANDARIN_WIDTH, BANANA_WIDTH, BLUEBERRY_WIDTH, STRAWBERRY_WIDTH};
@@ -86,13 +116,16 @@ void erase_basket(int,int);
 void draw_basket(int, int);
 void draw_fruit(int, int, int, int, int*);
 void erase_fruit(int, int, int, int);
+//void config_timer(void);
+//void interrupt_handler(void);
+//void TIMER_ISR(void);
 
 /******************************************************************************
  * This program draws rectangles and boxes on the VGA screen, and moves them
  * in an animated way.
  *****************************************************************************/
 int main(void){
-
+	volatile int *TIMER_ptr = (int *) TIMER_BASE;
     volatile int * pixel_ctrl_ptr = (int *) PIXEL_BUF_CTRL_BASE; // pixel controller
 	volatile int *LEDR_ptr = LEDR_BASE;
     volatile int *KEY_ptr = KEY_BASE;
@@ -119,97 +152,144 @@ int main(void){
     pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
     clear_screen();
 	initializer();
+	//config_timer();
+	srand(time(0));
+	
+	/* set interrupt mask bits for levels 0 (interval timer)  */
+	//NIOS2_WRITE_IENABLE(0x0);
+	//NIOS2_WRITE_STATUS(0); // enable Nios II interrupts
 
     while (1)
-    {
+    {	
+		// erases basket that was drawn in the last iteration
 		erase_basket(prev_prev_basket_x, prev_prev_basket_y);
 		prev_prev_basket_x = prev_basket_x;
 		prev_prev_basket_y = prev_basket_y;
 		prev_basket_x = basket_x_pos;
-		prev_basket_y = basket_y_pos; 
-		
-		erase_fruit(prev_prev_fruit_x, prev_prev_fruit_y, fruit_width[ran_num], fruit_height[ran_num]);
+		prev_basket_y = basket_y_pos;
+
+		/* Erase any fruits and baskets that were drawn in the last iteration */
+		erase_fruit(prev_prev_fruit_x, prev_prev_fruit_y, fruit_width[prev_prev_rand_fruit], fruit_height[prev_prev_rand_fruit]);
 		prev_prev_fruit_y = prev_fruit_y;
 		prev_prev_fruit_x = prev_fruit_x;
-
 		prev_fruit_y = fruit_y_pos;
 		prev_fruit_x = fruit_x_pos;
-
 		
-		while(fruit_y_pos < 240){
-	
-			erase_basket(prev_prev_basket_x, prev_prev_basket_y);
-			draw_basket(basket_x_pos, basket_y_pos);
-			prev_prev_basket_x = prev_basket_x;
-			prev_prev_basket_y = prev_basket_y;
-			prev_basket_x = basket_x_pos;
-			prev_basket_y = basket_y_pos; 
+		prev_prev_rand_fruit = prev_rand_fruit;
+		prev_rand_fruit = rand_fruit;
+
+		// draws new basket and fruit location
+		draw_basket(basket_x_pos, basket_y_pos);
+		draw_fruit(fruit_x_pos, fruit_y_pos, fruit_width[rand_fruit], fruit_height[rand_fruit], fruit_map[rand_fruit]);
 		
-			int edgecapture_bit = *(KEY_ptr + 3) & 0b11;
-			if(edgecapture_bit == 1){ // if key 0 is pressed
-				basket_x_pos += 20;
-				if(basket_x_pos > RESOLUTION_X - BASKET_WIDTH){
-					basket_x_pos -= 20;
-				}
-				*(KEY_ptr + 3) = 0xFF; // reset edge capture bit
-			}
-			else if(edgecapture_bit == 2){ // if key 1 is pressed
-				basket_x_pos-= 20;
-				if(basket_x_pos < 0){
-					basket_x_pos+= 20;
-				}
-				*(KEY_ptr + 3) = 0xFF; // reset edge capture bit
-			}
-			/* Erase any fruits and baskets that were drawn in the last iteration */
-			erase_fruit(prev_prev_fruit_x, prev_prev_fruit_y, fruit_width[ran_num], fruit_height[ran_num]);
-			draw_fruit(fruit_x_pos, fruit_y_pos, fruit_width[ran_num], fruit_height[ran_num], fruit_map[ran_num]);
-
 		
-			prev_prev_fruit_y = prev_fruit_y;
-			prev_prev_fruit_x = prev_fruit_x;
-
-			prev_fruit_y = fruit_y_pos;
-			prev_fruit_x = fruit_x_pos;
-
-			//if(fruit_x_pos - WATERMELON_WIDTH == basket_x_pos && fruit_y_pos - ){
-			//	continue; this is code for setting fruit in basket
+		//update next fruit location due to timer
+		fruit_y_pos = fruit_y_pos + drop_speed;
+		
+		
+		// if a catch is registered
+		
+		if(fruit_x_pos > basket_x_pos && fruit_x_pos < basket_x_pos + BASKET_WIDTH && fruit_y_pos == RESOLUTION_Y - BASKET_HEIGHT){
+			total_score++;
+			erase_fruit(prev_prev_fruit_x, prev_prev_fruit_y, fruit_width[prev_prev_rand_fruit], fruit_height[prev_prev_rand_fruit]);
+			erase_fruit(prev_fruit_x, prev_fruit_y, fruit_width[prev_rand_fruit], fruit_height[prev_rand_fruit]);
+			erase_fruit(fruit_x_pos, fruit_y_pos, fruit_width[rand_fruit], fruit_height[rand_fruit]);
 			
-			int SW_value = *(SW_ptr);
-			switch (SW_value) {
-				case 0b1:
-					fruit_y_pos += 2;
-				break;
-				case 0b10:
-					fruit_y_pos += 4;
-					
-				break;
-				case 0b100:
-					fruit_y_pos += 8;
-					
-				break;
-				case 0b1000:
-					fruit_y_pos += 16;
-				break;
-				default:
-					fruit_y_pos++;
-				break;
+			rand_fruit = rand() % 8;	
+			fruit_y_pos = 0;
+			fruit_x_pos = rand() % (RESOLUTION_X - 1);
+
+		}
+		
+		// drops a different fruit at a new location after the fruit hits the bottom of the screen
+		else if(fruit_y_pos > RESOLUTION_Y - BASKET_HEIGHT/2){
+			erase_fruit(prev_prev_fruit_x, prev_prev_fruit_y, fruit_width[prev_prev_rand_fruit], fruit_height[prev_prev_rand_fruit]);
+			erase_fruit(prev_fruit_x, prev_fruit_y, fruit_width[prev_rand_fruit], fruit_height[prev_rand_fruit]);
+			erase_fruit(fruit_x_pos, fruit_y_pos, fruit_width[rand_fruit], fruit_height[rand_fruit]);
+			
+			rand_fruit = rand() % 8;	
+			fruit_y_pos = 0;
+			fruit_x_pos = rand() % (RESOLUTION_X - 1);
+		}	
+			
+		// code that controls the switches
+		int edgecapture_bit = *(KEY_ptr + 3) & 0b11;
+		if(edgecapture_bit == 1){ // if key 0 is pressed
+			basket_x_pos += 20;
+			if(basket_x_pos > RESOLUTION_X - BASKET_WIDTH){
+				basket_x_pos -= 20;
 			}
-			wait_for_vsync(); // swap front and back buffers on VGA vertical sync
-			pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+			*(KEY_ptr + 3) = 0xFF; // reset edge capture bit
+		}
+		else if(edgecapture_bit == 2){ // if key 1 is pressed
+			basket_x_pos-= 20;
+			if(basket_x_pos < 0){
+				basket_x_pos+= 20;
+			}
+			*(KEY_ptr + 3) = 0xFF; // reset edge capture bit
 		}
 		
-		ran_num++;
-		if(ran_num > 8){
-			ran_num = 0;	
-		}
-		fruit_y_pos = 0;
-		fruit_x_pos = rand() % 8;
-		
+
+		// controls the speed of the fruit dropping
+		/*int SW_value = *(SW_ptr);
+		switch (SW_value) {
+			case 0b1:
+				if(fruit_y_pos > 240){
+					break;
+				}
+				else{
+					fruit_y_pos += 2;
+				}
+				break;
+			case 0b10:
+				if(fruit_y_pos > 240){
+					break;
+				}
+				else{
+					fruit_y_pos += 4;
+				}
+				break;
+			case 0b100:
+				if(fruit_y_pos > 240){
+					break;
+				}
+				else{
+					fruit_y_pos += 8;
+				}
+				break;
+			case 0b1000:
+				if(fruit_y_pos > 240){
+					break;
+				}
+				else{
+					fruit_y_pos += 16;
+				}
+				break;
+			default:
+				if(fruit_y_pos > 240){
+					break;
+				}
+				else{
+					fruit_y_pos++;
+				}
+				break;
+		}*/
+
+		wait_for_vsync(); // swap front and back buffers on VGA vertical sync
+		pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer	
 	}
-	
 }
 
 // code for subroutines (not shown)
+
+/*void config_timer(){
+	volatile int *TIMER_ptr = (int *) TIMER_BASE;
+	*(TIMER_ptr) = 0; //Resetting TO
+	*(TIMER_ptr + 2) = COUNTER_VALUE & 0xFFFF;
+	*(TIMER_ptr + 3) = (COUNTER_VALUE >> 16) & 0xFFFF;
+	*(TIMER_ptr + 1) = 0b0000; // turns on start, cont, and interrupt
+}*/
+
 
 void erase_fruit(int x, int y, int fruit_width, int fruit_height){
 	for (int i = y ; i < y + fruit_height; i++){ // i is basket height
@@ -246,14 +326,14 @@ void erase_basket(int x, int y){
 
 void initializer(){
 	
-	int ran_num = rand() % 8;
+	int rand_fruit = rand() % 8;
 	// initialzies basket start position
 	basket_y_pos = RESOLUTION_Y - BASKET_HEIGHT;
 	basket_x_pos = (RESOLUTION_X/2) - (BASKET_WIDTH/2);
 	fruit_y_pos = 0;
 	fruit_x_pos = rand() % (RESOLUTION_X - 1);
-	
-	
+	total_score = 0;
+	drop_speed = 1;
 	
 }
 
@@ -400,12 +480,73 @@ void erase_box(int x0, int y0){
 void wait_for_vsync(){
 	volatile int *pixel_ctrl_ptr = (int*)PIXEL_BUF_CTRL_BASE;
 	int status;
-	*pixel_ctrl_ptr = 1; // this will start the sync process, write 1 into the front buf
+	*(pixel_ctrl_ptr) = 1; // this will start the sync process, write 1 into the front buf
 	status = *(pixel_ctrl_ptr + 3);
 	
 	while((status & 0x01) != 0){ // polling the status bit, the status bit will turn to 0 when front buffer is done rendering
 		status = *(pixel_ctrl_ptr + 3);
 	}
 }
+
+// interrupt code that i cant figure out
+
+
+/*
+void interrupt_handler(){
+	int ipending;
+	NIOS2_READ_IPENDING(ipending);
+	if (ipending & 0x1){ // interval timer interrupts
+		TIMER_ISR();
+	}
+	return;
+}
+
+void TIMER_ISR(){ // we can honestly also consider playing music when it speeds up but it need to speed up slower
+	volatile int *TIMER_ptr = (int *) TIMER_BASE;
+	*(TIMER_ptr) = 0; // clears the interrupt
+	//drop_speed++;
+	printf("%d", 999);
+	return;
+}
+
+
+void the_exception(void) __attribute__((section(".exceptions")));
+void the_exception(void)
+{
+	asm("subi sp, sp, 128");
+	asm("stw et, 96(sp)");
+	asm("rdctl et, ctl4");
+	asm("beq et, r0, SKIP_EA_DEC"); // Interrupt is not external
+	asm("subi ea, ea, 4"); 
+	asm("SKIP_EA_DEC:");
+	asm("stw r1, 4(sp)"); // Save all registers
+	asm("stw r2, 8(sp)");
+	asm("ldw r19, 76(sp)");
+	asm("ldw r20, 80(sp)");
+	asm("ldw r21, 84(sp)");
+	asm("ldw r22, 88(sp)");
+	asm("ldw r23, 92(sp)");
+	asm("ldw r24, 96(sp)");
+	asm("ldw r25, 100(sp)"); // r25 = bt
+	asm("ldw r26, 104(sp)"); // r26 = gp
+	// skip r27 because it is sp, and we did not save this on the stack
+	asm("ldw r28, 112(sp)"); // r28 = fp
+	asm("ldw r29, 116(sp)"); // r29 = ea
+	asm("ldw r30, 120(sp)"); // r30 = ba
+	asm("ldw r31, 124(sp)"); // r31 = ra
+	asm("addi sp, sp, 128");
+	asm("eret");
+}
+
+void the_reset(void) __attribute__((section(".reset")));
+void the_reset(void)
+{
+	asm(".set noat"); 
+	asm(".set nobreak"); 
+	asm("movia r2, main"); // Call the C language main program
+	asm("jmp r2");
+}
+
+*/
 
 
